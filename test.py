93495a515d4514965f07df08bff50a25207c386a
Jonathan150305 <jonathanbernard265@gmail.com>
2020-04-11 18:15:00 +0700

    first commit

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-from tkinter import *  
+git config --global user.email "jonathanbernard265@gmail.com"
+git config --global user.name "Jonathan150305"
 
-
-screen= Tk()
-entry = Entry(screen, width=60, borderwidth=5)
-entry.grid(row=0, column=0, columnspan=2
-
-def button_mm():
-    number = entry.get
-
-
-
-
-
-button_mm=Button(screen, text="mm", padx=40, pady=20, command= button_mm) 
-button_cm=Button(screen, text="cm", padx=40, pady=20, command= button_cm) 
-
-button_mm.grid(row=1, column=0 )
-button_cm.grid(row=1, column=1 )
 
 
 
