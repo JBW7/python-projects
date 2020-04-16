d79b2974c9d0168ad2bbf8b073c0d99ce59e61ed
Jonathan150305 <jonathanbernard265@gmail.com>
2020-04-16 22:42:16 +0700

    defined buttons

--- tesla_viewer.py
+++ tesla_viewer.py
@@ -2,7 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 screen = Tk()
-screen.geometry("350x500")
+screen.geometry("750x750")
 screen.title("Image")
 
 # screen icon
@@ -16,14 +16,64 @@ model_y = ImageTk.PhotoImage(Image.open("d:/JOEY/python_pics/model_y.JPG"))
 roadster = ImageTk.PhotoImage(Image.open("d:/JOEY/python_pics/roadster.JPG"))
 cybertruck = ImageTk.PhotoImage(Image.open("d:/JOEY/python_pics/cybertruck.JPG"))
 
+
 img_list = [model_s, model_3, model_x, model_y, roadster, cybertruck ]
 
 # insert image to screen
-label = Label(image = img)
+label = Label(image = model_s)
 label.grid(row = 0, column = 0, columnspan = 3)
 
-#buttons
-back_button = Button(screen, text = "<<")
+# def button
+def back(image_number):
+	global label
+	global back_button
+	global next_button
+
+	label.grid_forget()
+	label.grid_forget()
+	label = Label(image = img_list[image_number - 1])
+	next_button = Button(screen, text = ">>", command = lambda: next(image_number + 1))
+	back_button = Button(screen, text = "<<", command = lambda: back(image_number - 1))
+
+	if image_number == 1 :
+		back_button = Button(screen, text = "<<", state = DISABLED)
+
+	
+	label.grid(row = 0, column = 0, columnspan = 3)
+	back_button.grid(row = 1, column = 0)
+	next_button.grid(row = 1, column = 2)
+
+
+def next(image_number):
+	global label
+	global back_button
+	global next_button
+
+	label.grid_forget()
+	label = Label(image = img_list[image_number - 1])
+	next_button = Button(screen, text = ">>", command = lambda: next(image_number + 1))
+	back_button = Button(screen, text = "<<", command = lambda: back(image_number - 1))
+
+	if image_number == 6 :
+		next_button = Button(screen, text = ">>", state = DISABLED)
+
+	label.grid(row = 0, column = 0, columnspan = 3)
+	back_button.grid(row = 1, column = 0)
+	next_button.grid(row = 1, column = 2)
+
+
+
+
+
+# buttons
+back_button = Button(screen, text = "<<", command = back, state = DISABLED)
+exit_button = Button(screen, text = "EXIT PROGRAM", command = screen.quit)
+next_button = Button(screen, text = ">>", command = lambda: next(2))
+
+back_button.grid(row = 1, column = 0)
+exit_button.grid(row = 1, column = 1)
+next_button.grid(row = 1, column = 2, pady = 10)
+
 
 
 
