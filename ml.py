8505eab777c720f3c5fd89012c4e6508f6b56828
Jonathan150305 <jonathanbernard265@gmail.com>
2020-05-18 02:49:52 +0700

    initial commit

--- ml.py
+++ ml.py
@@ -10,8 +10,8 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+xs = np.array([0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([4.0, 11.0, 18.0, 25.0, 32.0], dtype=float)
 
 model.fit(xs, ys, epochs= 500)
 
