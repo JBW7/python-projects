47985fdad07d27408112326332cd2c5d594ef2b6
Jonathan150305 <jonathanbernard265@gmail.com>
2020-10-21 12:40:38 +0700

    initial commit

--- tetris.py
+++ tetris.py
@@ -125,15 +125,15 @@ shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 16
 
 class Piece(object):  # *
     def __init__(self, x, y, shape):
-        self.x = x
-        self.y = y
+        self.x = int(x)
+        self.y = int(y)
         self.shape = shape
         self.color = shape_colors[shapes.index(shape)]
         self.rotation = 0
 
 
 def create_grid(locked_pos={}):  # *
-    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+    grid = [[int(0,0,0) for _ in range(10)] for _ in range(20)]
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
